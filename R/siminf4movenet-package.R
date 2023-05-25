55d4b8805ca63c9fa8c99e20feb655de0f980139
Matt Denwood <md@sund.ku.dk>
2023-05-25 15:54:35 +0200

    Migration from movenet

---
+++ R/siminf4movenet-package.R
@@ -0,0 +1,27 @@
+#' @name siminf4movenet-package
+#' @aliases siminf4movenet
+#' @title What the Package Does (One Line, Title Case)
+#'
+#' @details
+#' Some description of the package
+#'
+#' @examples
+#' ## An example function:
+#' df <- example_function()
+#' df
+#'
+#' ## To cite this package in publications use:
+#' citation("siminf4movenet")
+#'
+#' @seealso
+#' \code{\link[movenet]{movedata2networkDynamic}} for something in movenet
+#'
+#' @references
+#' Some article that you might want users to look at
+#'
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
